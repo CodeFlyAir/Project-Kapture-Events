36ffcb3fe7e3d1ba7f8b110fc4510a8adee3c8d7
Rungshit Saha <95049059+CodeFlyAir@users.noreply.github.com>
2024-03-11 02:31:53 +0530

    Added API Health

---
+++ KaptureEvents/src/main/java/com/kaptureevents/KaptureEvents/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.kaptureevents.KaptureEvents.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping("/check")
+    public ResponseEntity<String> health(){
+        return ResponseEntity.ok().body("Service Healthy");
+    }
+}
