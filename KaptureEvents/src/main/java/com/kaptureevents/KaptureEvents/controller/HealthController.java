cb26abf068113d10df99a315ada38142b7f5649a
Rungshit Saha <95049059+CodeFlyAir@users.noreply.github.com>
2024-03-18 01:28:57 +0530

    CORS update

--- KaptureEvents/src/main/java/com/kaptureevents/KaptureEvents/controller/HealthController.java
+++ KaptureEvents/src/main/java/com/kaptureevents/KaptureEvents/controller/HealthController.java
@@ -5,7 +5,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/health")
-@CrossOrigin(methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@CrossOrigin(
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS},
+        allowedHeaders = "*",
+        allowCredentials = "true",
+        origins = {"http://localhost:5174", "http://kapture-events.onrender.com"}
+)
 public class HealthController {
     @GetMapping("/check")
     public ResponseEntity<String> health(){
